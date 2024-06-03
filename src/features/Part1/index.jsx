ca2e57c970ccaca61d84b09c81cb731ba1eaecf9
Chanh Minh Nguyen <nguyen.chanh@hitachivantara.com>
2024-06-03 10:52:48 +0700

    update

--- src/features/Part1/index.jsx
+++ src/features/Part1/index.jsx
@@ -5,7 +5,7 @@ import bubble from '../../image/bubble.png';
 import actorButterfly from '../../image/actor_butterfly.gif';
 
 function Part1() {
-
+  const classHide = 'hide'
   var resource = {
     actor_anim: "https://media2.giphy.com/media/v1.Y2lkPTc5MGI3NjExdmlrczl1ZWNucGNoazIzOXJuZWIxb20zZzRtc2R3bHlpMmhpdXBmMSZlcD12MV9pbnRlcm5hbF9naWZfYnlfaWQmY3Q9cw/kPYi6lrYDnSttCDFih/giphy.gif",
     actor_static: actorStatic,
@@ -18,16 +18,17 @@ function Part1() {
     translateX : 0,
     counterTranslateX : 10,
     left : 11,
-    counterLeft: 0.10000
-
+    counterLeft: 0.10000,
+    index: 0
   }
   document.addEventListener('keydown', function(event) {
     const translateX = document.getElementById('translateX');
-    const image = document.getElementById('actor-content');
-    console.log(data.left);
     switch (event.keyCode) {
       case 39:
-        checkTranslateX();
+        if(!isValidTranslate(39)){
+          break;
+        }
+        data.index ++;
         var actorContent = document.getElementById('actor-content') ;
         if(actorContent.getAttribute('src')!==resource.actor_anim){
           actorContent.setAttribute('src',resource.actor_anim)
@@ -37,6 +38,10 @@ function Part1() {
         translateX.style.backgroundPosition = `${data.left}% center`;
         break;
       case 37:
+        if(!isValidTranslate(37)){
+          break;
+        }
+        data.index --;
         var actorContent = document.getElementById('actor-content') ;
         if(actorContent.getAttribute('src')!==resource.actor_anim){
           actorContent.setAttribute('src',resource.actor_anim)
@@ -60,13 +65,54 @@ function Part1() {
         break;
     }
   });
-  var checkTranslateX = () =>{
-      if(data.left == "-10.0"){
-        console.log("la la la la");
-        return 0;
-      }
+  var isValidTranslate = (keyCode)=>{
+    console.log(data.index);
+    console.log(keyCode);
+    switch (keyCode) {
+      case 39:
+        if(data.index == 100){
+          showActorButterfly();
+          return false;
+        }else{
+          hideActorButterfly();
+          return true;
+        }
+        break;
+      case 37:
+        if(data.index == 0){
+          return false;
+        }else{
+          hideActorButterfly();
+          return true;
+        }
+        break;
+      default:
+        break;
+    }
+
+    
   }
 
+  var showActorButterfly = ()=>{
+    var actor2 = document.getElementById('actor-2')
+    if(classExists(classHide)){
+        actor2.classList.remove(classHide)
+        // actor2.classList.add(classHide)
+    }
+  }
+
+  var hideActorButterfly = ()=>{
+    var actor2 = document.getElementById('actor-2')
+    if(!classExists(classHide)){
+        actor2.classList.add(classHide)
+    }
+  }
+
+  // var checkTranslateX = () =>{
+  // }
+  function classExists(className) {
+    return document.querySelector("." + className) !== null;
+  }
   return (
     <div className='container' id='translateX'>
       <div className='wrap'>
@@ -85,7 +131,7 @@ function Part1() {
             </div>
 
         </div>
-        <div className='actor-2'>
+        <div id='actor-2' className='hide'>
             <div className='wrap-text-bubble'>
               <div id='bubble'
                 style={{ backgroundImage: `url(${resource.bubble})` }}
