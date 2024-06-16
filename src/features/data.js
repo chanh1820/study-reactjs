dc413d60472ae8eb19b38b0a16118dacf9084d8a
chanh1820 <2kchanh@gmail.com>
2024-06-17 01:06:51 +0700

    update slide fro scratch

--- src/features/data.js
+++ src/features/data.js
@@ -8,27 +8,27 @@ const data = {
     },
     game1:{
         buttonPrevius:'/part1',
-        link:'<iframe src="https://1drv.ms/p/c/5234e06f93d9cd53/IQO2rUi_OtCsSbUHIvW_35JDAYRvtA4t0OOm9O-HJN9hpEU?em=2&amp;wdAr=1.3333333333333333" width="100%" height="100%" frameborder="0">Đây là tài liệu <a target="_blank" href="https://office.com">Microsoft Office</a> được nhúng, hoạt động trên nền tảng <a target="_blank" href="https://office.com/webapps">Office</a>.</iframe>',
+        link:'<iframe src="https://scratch.mit.edu/projects/1036936172/embed" allowtransparency="true" width="100%" height="100%" frameborder="0" scrolling="no" allowfullscreen></iframe>',
         buttonNext:"/game2"
     },
     game2:{
         buttonPrevius:'/game1',
-        link:'<iframe src="https://1drv.ms/p/c/5234e06f93d9cd53/IQP8MxOXeqWbSaLpl9WIPmeKAaSb9jLfi6XIaBVClNIXI30" width="100%" height="100%" frameborder="0" scrolling="no"></iframe>',
+        link:'<iframe src="https://scratch.mit.edu/projects/1037254737/embed" allowtransparency="true" width="100%" height="100%" frameborder="0" scrolling="no" allowfullscreen></iframe>',
         buttonNext:"/game3"
     },
     game3:{
         buttonPrevius:'/game2',
-        link:'<iframe src="https://1drv.ms/p/c/5234e06f93d9cd53/IQPFLTRs_t3pQqVqL8QLKeAPATYSCgKBo6HpLG4ZzPR7yAM?em=2&amp;wdAr=1.3333333333333333" width="100%" height="100%" frameborder="0">Đây là tài liệu <a target="_blank" href="https://office.com">Microsoft Office</a> được nhúng bản trình bày, hoạt động trên nền tảng <a target="_blank" href="https://office.com/webapps">Office</a>.</iframe>',
+        link:'<iframe src="https://scratch.mit.edu/projects/1037793502/embed" allowtransparency="true" width="100%" height="100%" frameborder="0" scrolling="no" allowfullscreen></iframe>',
         buttonNext:"/game4"
     },
     game4:{
         buttonPrevius:'/game3',
-        link:'<iframe src="https://1drv.ms/p/c/5234e06f93d9cd53/IQOEIkqgzzhoRJtNzOjoT77mARtxKFItLfnTIqpedXFrxpA?em=2&amp;wdAr=1.3333333333333333" width="100%" height="100%" frameborder="0">Đây là tài liệu <a target="_blank" href="https://office.com">Microsoft Office</a> được nhúng bản trình bày, hoạt động trên nền tảng <a target="_blank" href="https://office.com/webapps">Office</a>.</iframe>',
+        link:'<iframe src="https://scratch.mit.edu/projects/1038094672/embed" allowtransparency="true" width="100%" height="100%" frameborder="0" scrolling="no" allowfullscreen></iframe>',
         buttonNext:"/game5"
     },
     game5:{
         buttonPrevius:'/game4',
-        link:'<iframe src="https://1drv.ms/p/c/5234e06f93d9cd53/IQPGaPl1nJvJSZAq-6okYaqEAQVCAQdEIXEUO0PJYaB4VE8?em=2&amp;wdAr=1.3333333333333333" width="100%" height="100%" frameborder="0">Đây là tài liệu <a target="_blank" href="https://office.com">Microsoft Office</a> được nhúng bản trình bày, hoạt động trên nền tảng <a target="_blank" href="https://office.com/webapps">Office</a>.</iframe>',
+        link:'<iframe src="https://scratch.mit.edu/projects/1038123469/embed" allowtransparency="true" width="100%" height="100%" frameborder="0" scrolling="no" allowfullscreen></iframe>',
         buttonNext:"/part1"
     }
 }
